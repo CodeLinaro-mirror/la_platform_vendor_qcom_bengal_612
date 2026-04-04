89bb02bc522f0b7d270398256c2f9524a63b00d4
Arvind Kumar <c_karvin@qti.qualcomm.com>
2026-04-04 10:36:23 +0530

    bengal_612: Enable AVF for bengal
    We are enabling AVF for bengal by including a makefile.
    
    Change-Id: Ic37d8a44b868ccf64c2a74150cc785216d2cab0e

--- bengal_612.mk
+++ bengal_612.mk
@@ -445,6 +445,9 @@ FEATURE_GPS_LOC_QSH := false
 # Enable support for APEX updates
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+#Enable virtualization service
+$(call inherit-product, packages/modules/Virtualization/build/apex/product_packages.mk)
+
 ###################################################################################
 # This is the End of target.mk file.
 # Now, Pickup other split product.mk files:
